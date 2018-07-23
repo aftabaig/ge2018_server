61ef0f6081131f000f04070bbb32a51ea876d8dc
Aftab Baig <aftabaig@gmail.com>
2018-07-23 16:03:24 +0500

    pm2 settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,23 +4,10 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
   apps : [{
-      name      : 'tabdeeli_api',
+      name      : 'ge2018',
       script    : 'bin/www.js',
       env: {
         NODE_ENV: 'development'
-      },
-      env_staging : {
-        NODE_ENV: 'staging'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
-    },
-    {
-      name      : 'tabdeeli_worker',
-      script    : 'bin/worker.js',
-      env: {
-          NODE_ENV: 'development'
       },
       env_staging : {
         NODE_ENV: 'staging'
@@ -37,12 +24,12 @@ module.exports = {
    */
   deploy : {
     staging : {
-      key  : '/Users/aftab.akhtar/.ssh/tabdeeli',
-      user : 'aftab',
-      host : 'tabdeeliapp.ssasoft.com',
+      key  : '/Users/ssasoft/.ssh',
+      user : 'root',
+      host : '188.40.167.174',
       ref  : 'origin/master',
-      repo : 'git@git.ssasoft.com:tabdeeli/api.git',
-      path : '/home/aftab/tabdeeli',
+      repo : 'git@github.com:aftabaig/ge2018_server.git',
+      path : '/root/ge2018',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env staging'
     }
   }
